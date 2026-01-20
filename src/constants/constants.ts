4fb9161abb65bd88a7d466c18153679a8c922f81
Dushan Ranasinghage <dushanrandika1994@gmail.com>
2026-01-20 20:34:21 +1300

    Introduce discount calculation function to total calculation

---
+++ src/constants/constants.ts
@@ -0,0 +1,14 @@
+/**
+ * @file constants.ts
+ * @description
+ * @author Dushan Ranasinghage
+ * @copyright Copyright 2026 - All Rights Reserved.
+ */
+
+export const DISCOUNT_LEVELS = [
+  { threshold: 50000, rate: 0.15 },
+  { threshold: 10000, rate: 0.1 },
+  { threshold: 7000, rate: 0.07 },
+  { threshold: 5000, rate: 0.05 },
+  { threshold: 1000, rate: 0.03 },
+];
