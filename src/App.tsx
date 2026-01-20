216a2acdbc29d99c0ff914aa256dbd0f9d090f50
Dushan Ranasinghage <dushanrandika1994@gmail.com>
2026-01-20 19:52:49 +1300

    Add minimal styling to the form

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,45 @@
  * @copyright Copyright 2026 - All Rights Reserved.
  */
 
-import { Fragment } from "react/jsx-runtime";
-
 function App() {
   return (
-    <section>
-      <h1 className="text-3xl font-bold">Retail Calculator</h1>
-      <div>
-        <label htmlFor="numberOfItems">Number of Items:</label>
-        <input type="number" id="numberOfItems" name="numberOfItems" />
-      </div>
-      <div>
-        <label htmlFor="price">Price:</label>
-        <input type="number" id="price" name="price" />
-      </div>
-      <div>
-        <label htmlFor="region">Region:</label>
-        <select id="region" name="region">
-          <option value="">Select a region</option>
-          <option value="AUK">AUK - Auckland (6.85%)</option>
-          <option value="WLG">WLG - Wellington (8.00%)</option>
-          <option value="WAI">WAI - Waikato (6.25%)</option>
-          <option value="CHC">CHC - Christchurch (4.00%)</option>
-          <option value="TAS">TAS - Tasmania (8.25%)</option>
-        </select>
-      </div>
-      <div>
-        <button type="button">Calculate</button>
+    <section className="p-4">
+      <h1 className="text-3xl font-bold mb-4">Retail Calculator</h1>
+      <div className="space-y-3">
+        <div>
+          <label htmlFor="numberOfItems">Number of Items:</label>
+          <input
+            type="number"
+            id="numberOfItems"
+            name="numberOfItems"
+            className="ml-2 border"
+          />
+        </div>
+        <div>
+          <label htmlFor="price">Price:</label>
+          <input
+            type="number"
+            id="price"
+            name="price"
+            className="ml-2 border"
+          />
+        </div>
+        <div>
+          <label htmlFor="region">Region:</label>
+          <select id="region" name="region" className="ml-2 border">
+            <option value="">Select a region</option>
+            <option value="AUK">AUK - Auckland (6.85%)</option>
+            <option value="WLG">WLG - Wellington (8.00%)</option>
+            <option value="WAI">WAI - Waikato (6.25%)</option>
+            <option value="CHC">CHC - Christchurch (4.00%)</option>
+            <option value="TAS">TAS - Tasmania (8.25%)</option>
+          </select>
+        </div>
+        <div>
+          <button type="button" className="px-4 py-1 border bg-gray-100">
+            Calculate
+          </button>
+        </div>
       </div>
     </section>
   );
